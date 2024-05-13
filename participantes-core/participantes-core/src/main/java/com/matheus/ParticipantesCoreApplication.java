42c8464ac2aa8c395ce8e5cf9a3c6f9f1a2344af
pedro.scattaregi@limberhealth.com <pedro.scattaregi@limberhealth.com>
2024-05-13 03:26:56 -0300

    feat: criacao do controller, model e conexao com o banco h2

--- participantes-core/participantes-core/src/main/java/com/matheus/ParticipantesCoreApplication.java
+++ participantes-core/participantes-core/src/main/java/com/matheus/ParticipantesCoreApplication.java
@@ -1,13 +1,34 @@
 package com.matheus;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.matheus.model.Participantes;
+import com.matheus.repository.ParticipantesRepository;
 
 @SpringBootApplication
 public class ParticipantesCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParticipantesCoreApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(ParticipantesRepository participantesRepository) {
+		return args -> {
+			participantesRepository.deleteAll();
+
+			Participantes p = new Participantes();
+			p.setNome("matheus");
+			p.setCpf("12312312312");
+			p.setTelefone("40028922");
+
+			participantesRepository.save(p);
+
+		};
+		
 	}
 
 }
