0912f04e860f5e50d6e16f8ca67c52d8a1179202
Matheus Brandao <brandao.matheus1000@gmail.com>
2024-05-13 14:30:29 -0300

    feat: adicionado metodo post

--- participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
+++ participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
@@ -2,8 +2,12 @@ package com.matheus.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.model.Participantes;
@@ -23,4 +27,10 @@ public class ParticipantesController {
         return participantesRepository.findAll();
     }
     
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Participantes create(@RequestBody Participantes participantes) {
+
+        return participantesRepository.save(participantes);
+    }
 }
