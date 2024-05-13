0ad6c68d3fe7511490b6defc21c3cc5a9e00a42c
pedro.scattaregi@limberhealth.com <pedro.scattaregi@limberhealth.com>
2024-05-12 23:11:46 -0300

    build: novo projeto estruturado

---
+++ participantes-core/participantes-core/src/test/java/com/matheus/ParticipantesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipantesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
