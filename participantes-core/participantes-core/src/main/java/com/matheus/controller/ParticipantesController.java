4f501c2e11fcd113046748b8ff38e16e09991aeb
Matheus Brandao <brandao.matheus1000@gmail.com>
2024-05-14 00:26:04 -0300

    feito o metodo de chamada de busca por id e chamada put para edicao de participante

--- participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
+++ participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
@@ -3,7 +3,9 @@ package com.matheus.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import com.matheus.model.Participantes;
 import com.matheus.repository.ParticipantesRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/participantes")
@@ -26,11 +29,35 @@ public class ParticipantesController {
     public List<Participantes> list() {
         return participantesRepository.findAll();
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Participantes> findById(@PathVariable Long id) {
+        return participantesRepository.findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Participantes create(@RequestBody Participantes participantes) {
 
         return participantesRepository.save(participantes);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Participantes>update(@PathVariable Long id,
+            @RequestBody Participantes participantes) {
+        return participantesRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setNome(participantes.getNome());
+                    recordFound.setCpf(participantes.getCpf());
+                    recordFound.setTelefone(participantes.getTelefone());
+                    recordFound.setSexo(participantes.getSexo());
+                    recordFound.setCivil(participantes.getCivil());
+                    recordFound.setObservacao(participantes.getObservacao());
+                    Participantes updated = participantesRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
