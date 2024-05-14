bc4d9abdada328f55f826bb7fecb4a0474302cb5
Matheus Brandao <brandao.matheus1000@gmail.com>
2024-05-14 13:35:10 -0300

    feat: feito metodo de delete

--- participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
+++ participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,16 @@ public class ParticipantesController {
                     recordFound.setObservacao(participantes.getObservacao());
                     Participantes updated = participantesRepository.save(recordFound);
                     return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return participantesRepository.findById(id)
+                .map(recordFound -> {
+                    participantesRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
