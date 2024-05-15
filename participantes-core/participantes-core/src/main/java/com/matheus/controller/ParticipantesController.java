d8aa71c184a405440553aff2d1c9981f9853f1d8
Matheus Brandao <brandao.matheus1000@gmail.com>
2024-05-15 03:38:56 -0300

    feat: Adicionado camada de servico e tratamento de excecoes

--- participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
+++ participantes-core/participantes-core/src/main/java/com/matheus/controller/ParticipantesController.java
@@ -3,7 +3,6 @@ package com.matheus.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,65 +14,50 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.model.Participantes;
-import com.matheus.repository.ParticipantesRepository;
+import com.matheus.service.ParticipanteService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @Validated
 @RestController
 @RequestMapping("/api/participantes")
-@AllArgsConstructor
 public class ParticipantesController {
 
-    private final ParticipantesRepository participantesRepository;
+    private final ParticipanteService participanteService;
+
+        public ParticipantesController (ParticipanteService participanteService) {
+        this.participanteService = participanteService;
+    }
 
     @GetMapping
     public List<Participantes> list() {
-        return participantesRepository.findAll();
+        return participanteService.list();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Participantes> findById(@PathVariable @NotNull @Positive Long id) {
-        return participantesRepository.findById(id)
-                .map(recordFound -> ResponseEntity.ok().body(recordFound))
-                .orElse(ResponseEntity.notFound().build());
+    public Participantes findById(@PathVariable @NotNull @Positive Long id) {
+        return participanteService.findById(id);               
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Participantes create(@RequestBody @Valid Participantes participantes) {
 
-        return participantesRepository.save(participantes);
+        return participanteService.create(participantes);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Participantes>update(@PathVariable @NotNull @Positive Long id,
+    public Participantes update(@PathVariable @NotNull @Positive Long id,
             @RequestBody @Valid Participantes participantes) {
-        return participantesRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setNome(participantes.getNome());
-                    recordFound.setCpf(participantes.getCpf());
-                    recordFound.setTelefone(participantes.getTelefone());
-                    recordFound.setSexo(participantes.getSexo());
-                    recordFound.setCivil(participantes.getCivil());
-                    recordFound.setObservacao(participantes.getObservacao());
-                    Participantes updated = participantesRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
-                .orElse(ResponseEntity.notFound().build());
+        return participanteService.update(id, participantes);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-        return participantesRepository.findById(id)
-                .map(recordFound -> {
-                    participantesRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        participanteService.delete(id);
     }
 }
